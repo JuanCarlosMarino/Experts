d7a67631b3d8fcfffd2fc9e3a8bf2566909f3c34
Ciro Gamboa <ciro.gamboa.2013@upb.edu.co>
2021-11-12 06:58:35 -0500

    Added API for updating and deleting Experts

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,34 @@ app.post('/experts', function(req, res){
     })
 })
 
+// Actualizar totalmente un experto en la DB
+app.put('/experts/:id', function(req, res){
+    const uid = req.params.id;
+    const expert = req.body;
+
+    dbE.updateExpertTotally(uid, expert, function(status){
+        res.json(status);
+    })
+})
+
+// Actualizar parcialmente un experto en la DB
+app.patch('/experts/:id', function(req, res){
+    const uid = req.params.id;
+    const expert = req.body;
+
+    dbE.updateExpertPartially(uid, expert, function(status){
+        res.json(status);
+    })
+})
+
+// Borrar experto de la base de datos
+app.delete('/experts/:id',function(req, res){
+    const uid = req.params.id;
+    dbE.deleteExpert(uid, function(status){
+        res.json(status);
+    })
+})
+
 app.listen(port, () => {
     console.log("Running on port " + port);
 })
