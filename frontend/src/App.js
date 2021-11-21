18f563a5e05d28983f2944ba75a8e0965cf0b1cf
Ciro Gamboa <ciro.gamboa.2013@upb.edu.co>
2021-11-21 15:48:48 -0500

    Navbar example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from "./components/NavBar";
 
 function App() {
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,12 +1,40 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const NavBar = (props) => {
     return(
         <div>
-            <h2>Holaaaa!! {props.nombre} </h2>
-            <h3>Apellido: {props.apellido}</h3>
+            <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
         </div>
     )
+}
+
+const styles = {
+    color : "red",
+    fontFamily : "Arial",
+    fontSize : "30px"
 }
 
 export default NavBar;
