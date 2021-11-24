209a9cd0154b9304a0c6917586a762a97cdb4908
Ciro Gamboa <ciro.gamboa.2013@upb.edu.co>
2021-11-24 07:52:40 -0500

    Added email/password login

--- frontend/src/components/SignInButton.js
+++ frontend/src/components/SignInButton.js
@@ -22,7 +22,10 @@ const uiConfig = {
   // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
   signInSuccessUrl: "/home",
   // We will display Google and Facebook as auth providers.
-  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
 
   callbacks: {
     signInSuccessWithAuthResult: function (authResult, redirectUrl) {
