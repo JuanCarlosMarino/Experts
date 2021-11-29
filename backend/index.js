12985ee87514d6954b2aec920331011d142a67b7
Juan Carlos Mariño Morantes <46898982+JuanCarlosMarino@users.noreply.github.com>
2021-11-29 17:24:26 -0500

    port

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const dbE = require("./src/db/crudExperts.js");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
