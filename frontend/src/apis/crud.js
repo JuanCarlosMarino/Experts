caa526c7373cde279157c503721cface17c3d354
Juan Carlos Mariño Morantes <46898982+JuanCarlosMarino@users.noreply.github.com>
2021-12-01 12:44:42 -0500

    Update crud.js

--- frontend/src/apis/crud.js
+++ frontend/src/apis/crud.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000";
+const baseUrl = "https://experts-backend-b13.herokuapp.com";
 
 export function searchExperts(city, callback) {
   axios.get(baseUrl + "/experts/search/" + city).then((res) => {
